c26a3ed90981a056cc50543941beb310d42b0e01
mongolyy <mongolyy@gmail.com>
2022-04-23 17:04:37 +0900

    next-authの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { signIn } from "next-auth/react"
 
 const Home: NextPage = () => {
   return (
@@ -20,6 +21,8 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
+
+          <button onClick={() => signIn()}>Sign in</button>
         </p>
 
         <div className={styles.grid}>
