7d280fdd399d43dfb8700e5d1fd7d40400fec86d
mongolyy <mongolyy@gmail.com>
2022-06-04 16:43:08 +0900

    トップページから不要なアイテムを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,14 @@
 import type { NextPage } from 'next'
-import { signIn } from 'next-auth/react'
 import Link from 'next/link'
 import Layout from '../components/layout'
 
 const Home: NextPage = () => {
   return (
     <Layout title="トップページ | company-library">
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
       <div>
-        <button onClick={() => signIn()}>Sign in</button>
-
-        <div>
-          <Link href="/Users/mongol/dev/company-library/src/pages/private">
-            <a>private page</a>
-          </Link>
-        </div>
+        <Link href="/Users/mongol/dev/company-library/src/pages/private">
+          <a>private page</a>
+        </Link>
       </div>
     </Layout>
   )
