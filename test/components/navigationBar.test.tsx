2c6221b56d73c722923c013ed20ccd0f094cfe2c
mongolyy <mongolyy@gmail.com>
2022-10-01 15:44:15 +0900

    テストの追加

--- test/components/navigationBar.test.tsx
+++ test/components/navigationBar.test.tsx
@@ -2,14 +2,30 @@ import { getByText, render } from '@testing-library/react'
 import NavigationBar from '@/components/navigationBar'
 
 describe('navigationBar component', () => {
-  jest.spyOn(require('next/router'), 'useRouter').mockReturnValue({ push: jest.fn() })
+  const routerMock = jest
+    .spyOn(require('next/router'), 'useRouter')
+    .mockReturnValue({ push: jest.fn() })
 
   it('ナビゲーション項目が表示される', () => {
     const { getByText } = render(<NavigationBar />)
 
     expect(getByText('company-library')).toBeInTheDocument()
     expect(getByText('書籍一覧')).toBeInTheDocument()
+    expect(getByText('書籍一覧')).not.toHaveClass('bg-gray-600')
     expect(getByText('登録')).toBeInTheDocument()
+    expect(getByText('登録')).not.toHaveClass('bg-gray-600')
     expect(getByText('未返却一覧')).toBeInTheDocument()
+    expect(getByText('未返却一覧')).not.toHaveClass('bg-gray-600')
+  })
+
+  it('pathが/books/registerの場合、登録ボタンのデザインが強調される', () => {
+    routerMock.mockReturnValueOnce({
+      push: jest.fn(),
+      pathname: '/books/register',
+    })
+
+    const { getByText } = render(<NavigationBar />)
+
+    expect(getByText('登録')).toHaveClass('bg-gray-600')
   })
 })
