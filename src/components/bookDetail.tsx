c7a7c83f7b41881721cec389cccc8ee3024e3780
katakoto291 <kakkookakoo@gmail.com>
2022-10-30 16:13:30 +0900

    貸出中のユーザーは別のリストで表示されるよう修正

--- src/components/bookDetail.tsx
+++ src/components/bookDetail.tsx
@@ -5,7 +5,7 @@ import ReturnButton from '@/components/returnButton'
 import { useCustomUser } from '@/hooks/useCustomUser'
 import { DateTime } from 'luxon'
 
-const dateFormat = 'yyyy-MM-dd'
+const dateFormat = 'yyyy/MM/dd'
 
 type BookDetailProps = {
   book: {
@@ -59,6 +59,14 @@ const BookDetail: FC<BookDetailProps> = ({ book }) => {
 
   const isLendable = !isLending && lendables > 0
 
+  const lendingHistories = book.lendingHistories
+    .filter((h) => h.returnHistories.length === 0)
+    .sort((h1, h2) => (h1.dueDate > h2.dueDate ? 1 : -1))
+
+  const lentHistories = book.lendingHistories
+    .filter((h) => h.returnHistories.length !== 0)
+    .sort((h1, h2) => (h1.returnHistories[0].createdAt < h2.returnHistories[0].createdAt ? 1 : -1))
+
   return (
     <div>
       <div>
@@ -82,33 +90,65 @@ const BookDetail: FC<BookDetailProps> = ({ book }) => {
         disabled={!isLending}
       />
 
-      <div>借りた人</div>
-      <table>
-        <thead>
-          <tr>
-            <th>返却予定日 or 返却日付</th>
-            <th>人</th>
-            <th>感想</th>
-          </tr>
-        </thead>
-        <tbody>
-          {book.lendingHistories.map((lendingHistory) => {
-            return (
-              <tr key={lendingHistory.id}>
-                <td>
-                  {DateTime.fromISO(
-                    lendingHistory.returnHistories[0]?.createdAt || lendingHistory.dueDate,
-                  )
-                    .setZone('Asia/Tokyo')
-                    .toFormat(dateFormat)}
-                </td>
-                <td>{lendingHistory.user.name}</td>
-                <td>{lendingHistory.user.impressions[0]?.impression}</td>
+      {lendingHistories.length > 0 && (
+        <>
+          <div>借りている人</div>
+          <table>
+            <thead>
+              <tr>
+                <th>返却予定日</th>
+                <th>人</th>
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {lendingHistories.map((lendingHistory, index) => {
+                return (
+                  <tr key={lendingHistory.id}>
+                    <td data-testid={`dueDate-${index}`}>
+                      {DateTime.fromISO(lendingHistory.dueDate)
+                        .setZone('Asia/Tokyo')
+                        .toFormat(dateFormat)}
+                    </td>
+                    <td data-testid={`lendingUser-${index}`}>{lendingHistory.user.name}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
+
+      <div>借りた人</div>
+      {lentHistories.length === 0 ? (
+        <div>いません</div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>返却日付</th>
+              <th>人</th>
+              <th>感想</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lentHistories.map((lendingHistory, index) => {
+              return (
+                <tr key={lendingHistory.id}>
+                  <td data-testid={`returnedDate-${index}`}>
+                    {DateTime.fromISO(lendingHistory.returnHistories[0].createdAt)
+                      .setZone('Asia/Tokyo')
+                      .toFormat(dateFormat)}
+                  </td>
+                  <td data-testid={`returnedUser-${index}`}>{lendingHistory.user.name}</td>
+                  <td data-testid={`impression-${index}`}>
+                    {lendingHistory.user.impressions[0]?.impression}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
--- test/components/bookDetail.test.tsx
+++ test/components/bookDetail.test.tsx
@@ -36,4 +36,114 @@ describe('BookDetail component', () => {
     expect(getByRole('button', { name: '借りる' })).toBeInTheDocument()
     expect(getByRole('button', { name: '返却する' })).toBeInTheDocument()
   })
+
+  describe('借りている人', () => {
+    it('貸出中のユーザーがいる場合、その一覧が返却予定日の昇順で表示される', () => {
+      const lendingBook = {
+        ...lendableBook,
+        reservations: [],
+        lendingHistories: [
+          {
+            id: 1,
+            createdAt: '2022-10-01',
+            user: { id: 1, name: 'user01', impressions: [] },
+            dueDate: '2022-10-24',
+            returnHistories: [],
+          },
+          {
+            id: 2,
+            createdAt: '2022-10-01',
+            user: { id: 2, name: 'user02', impressions: [] },
+            dueDate: '2022-10-08',
+            returnHistories: [],
+          },
+          {
+            id: 3,
+            createdAt: '2022-10-01',
+            user: { id: 3, name: 'user03', impressions: [] },
+            dueDate: '2022-10-15',
+            returnHistories: [],
+          },
+        ],
+      }
+
+      const { getByText, getByTestId } = render(<BookDetail book={lendingBook} />)
+
+      expect(getByText('借りている人')).toBeInTheDocument()
+      expect(getByTestId(`dueDate-${0}`).textContent).toBe('2022/10/08')
+      expect(getByTestId(`lendingUser-${0}`).textContent).toBe('user02')
+      expect(getByTestId(`dueDate-${1}`).textContent).toBe('2022/10/15')
+      expect(getByTestId(`lendingUser-${1}`).textContent).toBe('user03')
+      expect(getByTestId(`dueDate-${2}`).textContent).toBe('2022/10/24')
+      expect(getByTestId(`lendingUser-${2}`).textContent).toBe('user01')
+    })
+
+    it('貸出中のユーザーがいない場合、項目ごと表示されない', () => {
+      const { queryByText } = render(<BookDetail book={lendableBook} />)
+
+      expect(queryByText('借りている人')).not.toBeInTheDocument()
+    })
+  })
+
+  describe('借りた人', () => {
+    it('返却済の貸出履歴がある場合、その一覧が返却日の昇順で表示される', () => {
+      const returnedBook = {
+        ...lendableBook,
+        reservations: [],
+        lendingHistories: [
+          {
+            id: 1,
+            createdAt: '2022-10-01',
+            user: { id: 1, name: 'user01', impressions: [] },
+            dueDate: '2022-10-24',
+            returnHistories: [{ createdAt: '2022-10-20' }],
+          },
+          {
+            id: 2,
+            createdAt: '2022-10-01',
+            user: {
+              id: 2,
+              name: 'user02',
+              impressions: [
+                {
+                  impression: '感想を書きました',
+                  createdAt: '2022-10-30',
+                  updatedAt: '2022-10-30',
+                },
+              ],
+            },
+            dueDate: '2022-10-08',
+            returnHistories: [{ createdAt: '2022-10-30' }],
+          },
+          {
+            id: 3,
+            createdAt: '2022-10-01',
+            user: { id: 3, name: 'user03', impressions: [] },
+            dueDate: '2022-10-15',
+            returnHistories: [{ createdAt: '2022-10-25' }],
+          },
+        ],
+      }
+
+      const { getByText, getByTestId } = render(<BookDetail book={returnedBook} />)
+
+      expect(getByText('借りた人')).toBeInTheDocument()
+      expect(getByTestId(`returnedDate-${0}`).textContent).toBe('2022/10/30')
+      expect(getByTestId(`returnedUser-${0}`).textContent).toBe('user02')
+      expect(getByTestId(`impression-${0}`).textContent).toBe('感想を書きました')
+      expect(getByTestId(`returnedDate-${1}`).textContent).toBe('2022/10/25')
+      expect(getByTestId(`returnedUser-${1}`).textContent).toBe('user03')
+      expect(getByTestId(`impression-${1}`).textContent).toBe('')
+      expect(getByTestId(`returnedDate-${2}`).textContent).toBe('2022/10/20')
+      expect(getByTestId(`returnedUser-${2}`).textContent).toBe('user01')
+      expect(getByTestId(`impression-${2}`).textContent).toBe('')
+    })
+
+    it('返却済の貸出履歴がない場合、いないことが表示される', () => {
+      const { getByText } = render(<BookDetail book={lendableBook} />)
+
+      expect(getByText('借りた人')).toBeInTheDocument()
+      expect(getByText('いません')).toBeInTheDocument()
+    })
+  })
 })
