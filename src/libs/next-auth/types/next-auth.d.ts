7b4050e0348bef08a2e094015cd8640348a53d55
mongolyy <mongolyy@gmail.com>
2023-02-05 21:55:47 +0900

    next.jsのバージョンアップ

---
+++ src/libs/next-auth/types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import { User } from '@/models/user'
+
+declare module 'next-auth' {
+  interface Session {
+    customUser: User
+    idToken: string
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    idToken: string | undefined
+  }
+}
