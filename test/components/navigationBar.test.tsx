b52bc9cf7c9220ed3529e7f2a3505a11cc52d1a6
katakoto291 <kakkookakoo@gmail.com>
2023-02-05 15:03:17 +0900

    未返却一覧をクリックすると自身のプロフィール画面に遷移するよう変更

--- test/components/navigationBar.test.tsx
+++ test/components/navigationBar.test.tsx
@@ -1,10 +1,15 @@
-import { getByText, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import NavigationBar from '@/components/navigationBar'
+import { user1 } from '../__utils__/data/user'
 
 describe('navigationBar component', () => {
   const routerMock = jest
     .spyOn(require('next/router'), 'useRouter')
     .mockReturnValue({ push: jest.fn() })
+  const loggedInUser = user1
+  jest
+    .spyOn(require('@/hooks/useCustomUser'), 'useCustomUser')
+    .mockReturnValue({ user: loggedInUser })
 
   it('ナビゲーション項目が表示される', () => {
     const { getByText } = render(<NavigationBar />)
@@ -40,5 +45,24 @@ describe('navigationBar component', () => {
     const { getByText } = render(<NavigationBar />)
 
     expect(getByText('利用者一覧')).toHaveClass('bg-gray-600')
+  })
+
+  it('pathが/users/ログインユーザーのidの場合、未返却一覧ボタンのデザインが強調される', () => {
+    routerMock.mockReturnValueOnce({
+      push: jest.fn(),
+      asPath: `/users/${loggedInUser.id}`,
+    })
+
+    const { getByText, rerender } = render(<NavigationBar />)
+
+    expect(getByText('未返却一覧')).toHaveClass('bg-gray-600')
+
+    // ログインユーザー以外のIDの場合強調されない
+    routerMock.mockReturnValueOnce({
+      push: jest.fn(),
+      asPath: `/users/${loggedInUser.id + 1}`,
+    })
+    rerender(<NavigationBar />)
+    expect(getByText('未返却一覧')).not.toHaveClass('bg-gray-600')
   })
 })
