a07434ac53523ab78b7176c856e2962de2292e2b
katakoto291 <kakkookakoo@gmail.com>
2023-04-23 14:16:30 +0900

    standaloneがexperimentalではなくなったため、書き方を変更する

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  output: 'standalone',
   images: {
     domains: ['books.google.com', 's.gravatar.com'],
   },
