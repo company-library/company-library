c42fbe655551640e211c89f99f22633d66d65297
mongolyy <mongolyy@gmail.com>
2023-04-29 14:01:31 +0900

    CloudSQLの作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,15 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_sql_database_instance" "company-library" {
+  name             = "company-library-db"
+  database_version = "POSTGRES_14"
+  region           = "asia-northeast1"
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+}
