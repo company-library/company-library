6a4072101c559410bcac0e3118808a40ef8a2e86
mongolyy <mongolyy@gmail.com>
2023-04-29 15:38:03 +0900

    Hasura用のCloudRunを追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_sql_database_instance" "company-library" {
-  name             = "company-library-db"
+  name             = "cl-db"
   database_version = "POSTGRES_14"
   region           = "asia-northeast1"
 
@@ -31,4 +31,48 @@ resource "google_sql_database_instance" "company-library" {
     # type. See argument reference below.
     tier = "db-f1-micro"
   }
+}
+
+resource "google_vpc_access_connector" "company-library" {
+  name          = "cl-vpc-con"
+  ip_cidr_range = "10.8.0.0/28"
+  network       = google_compute_network.vpc_network.id
+}
+
+variable "db_user" {}
+variable "db_pass" {}
+variable "db_name" {}
+
+resource "google_cloud_run_service" "company-library-hasura" {
+  name     = "cl-hasura"
+  location = "asia-northeast1"
+
+  template {
+    spec {
+      containers {
+        image = "hasura/graphql-engine:v2.24.1"
+        env {
+          name  = "HASURA_GRAPHQL_DATABASE_URL"
+          value = "postgres://${var.db_user}:${var.db_pass}@${google_sql_database_instance.company-library.private_ip_address}:5432/${var.db_name}"
+        }
+        env {
+          name  = "HASURA_GRAPHQL_ENABLE_CONSOLE"
+          value = "true"
+        }
+        env {
+          name  = "PG_DATABASE_URL"
+          value = "postgres://${var.db_user}:${var.db_pass}@${google_sql_database_instance.company-library.private_ip_address}:5432/${var.db_name}"
+        }
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale"      = "100"
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.company-library.id
+        "run.googleapis.com/client-name"        = "terraform"
+      }
+    }
+  }
+  autogenerate_revision_name = true
 }
