c38abb7976324fca1d5a6797f205c03931009a0d
mongolyy <mongolyy@gmail.com>
2023-05-27 15:22:10 +0900

    dbをprivateに変更

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,15 +25,38 @@ variable "db_user" {}
 variable "db_pass" {}
 variable "db_name" {}
 
+resource "google_compute_global_address" "private_ip_address" {
+  provider = google
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google
+
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+
 resource "google_sql_database_instance" "company-library" {
   name             = "cl-db"
   database_version = "POSTGRES_14"
   region           = "asia-northeast1"
 
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
   settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
     tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled                                  = false
+      private_network                               = google_compute_network.vpc_network.id
+    }
   }
 }
 
