91dd37e8e3bfd24d7cfda7af57cda63c19dee3b1
mongolyy <mongolyy@gmail.com>
2023-07-10 22:17:08 +0900

    テストの失敗に対応

--- test/__utils__/data/user.ts
+++ test/__utils__/data/user.ts
@@ -1,19 +1,49 @@
-import { OldUserSummary } from '@/models/user'
+import { OldUserSummary, UserSummary } from '@/models/user'
 
-export const user1 = {
+export const user1: UserSummary = {
   id: 1,
   name: 'テスト太郎',
   email: 'test1@example.com',
   imageUrl: 'https://example.com/image/1',
-  createdAt: new Date('2023-01-01'),
+  lendingHistories: [
+    { bookId: 1, lentAt: new Date('2021-01-01'), returnHistory: null },
+    { bookId: 1, lentAt: new Date('2021-02-01'), returnHistory: null },
+    { bookId: 2, lentAt: new Date('2021-01-02'), returnHistory: null },
+    { bookId: 3, lentAt: new Date('2021-01-03'), returnHistory: null },
+    {
+      bookId: 3,
+      lentAt: new Date('2021-01-03'),
+      returnHistory: { returnedAt: new Date('2021-02-03') },
+    },
+    {
+      bookId: 4,
+      lentAt: new Date('2021-01-04'),
+      returnHistory: { returnedAt: new Date('2021-02-04') },
+    },
+    {
+      bookId: 4,
+      lentAt: new Date('2021-03-04'),
+      returnHistory: { returnedAt: new Date('2021-04-04') },
+    },
+    {
+      bookId: 5,
+      lentAt: new Date('2021-01-05'),
+      returnHistory: { returnedAt: new Date('2021-02-05') },
+    },
+    {
+      bookId: 6,
+      lentAt: new Date('2021-01-06'),
+      returnHistory: { returnedAt: new Date('2021-02-06') },
+    },
+  ],
 }
 
-export const user2 = {
+export const user2: UserSummary = {
   id: 2,
   name: 'テスト二郎',
   email: 'test2@example.com',
   imageUrl: null,
-  createdAt: new Date('2023-01-02'),
+  lendingHistories: [],
 }
 
 export const oldUser1: OldUserSummary = {
--- test/components/userCard.test.tsx
+++ test/components/userCard.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render } from '@testing-library/react'
 import UserCard from '@/components/userCard'
-import { oldUser1, oldUser2 } from '../__utils__/data/user'
+import { user1, user2 } from '../__utils__/data/user'
 
 jest.mock('next/link', () => ({
   __esModule: true,
@@ -11,20 +11,20 @@ jest.mock('next/link', () => ({
 
 describe('UserCard component', () => {
   it('ユーザー情報が表示されていること', () => {
-    const { getByText, getByTestId } = render(<UserCard user={oldUser1} />)
+    const { getByText, getByTestId } = render(<UserCard user={user1} />)
 
-    expect(getByText(oldUser1.name)).toBeInTheDocument()
-    expect(getByText(oldUser1.email)).toBeInTheDocument()
+    expect(getByText(user1.name)).toBeInTheDocument()
+    expect(getByText(user1.email)).toBeInTheDocument()
     expect(getByTestId('profileImage')).toBeInTheDocument()
-    expect(getByTestId('userProfileLink')).toHaveAttribute('href', `/users/${oldUser1.id}`)
+    expect(getByTestId('userProfileLink')).toHaveAttribute('href', `/users/${user1.id}`)
     expect(getByTestId('readingBookCount').textContent).toBe('3')
     expect(getByTestId('haveReadBookCount').textContent).toBe('4')
 
-    fireEvent.click(getByText(oldUser1.name))
+    fireEvent.click(getByText(user1.name))
   })
 
   it('ユーザー画像が存在しない場合は、ユーザー画像は表示されない', () => {
-    const { queryByTestId } = render(<UserCard user={oldUser2} />)
+    const { queryByTestId } = render(<UserCard user={user2} />)
 
     expect(queryByTestId('profileImage')).not.toBeInTheDocument()
   })
