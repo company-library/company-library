7df41e21590dede766434d93ae592061e9c5eb09
mongolyy <mongolyy@gmail.com>
2023-08-07 01:19:01 +0900

    [id]/bookListのテストの修正

--- test/app/users/id/bookList.test.tsx
+++ test/app/users/id/bookList.test.tsx
@@ -2,22 +2,9 @@ import { render } from '@testing-library/react'
 import { bookWithImage, bookWithoutImage } from '../../../__utils__/data/book'
 import BookList from '@/app/users/[id]/bookList'
 
-const books = [bookWithImage, bookWithoutImage]
-const useGetBooksQueryMock = jest.fn().mockReturnValue([
-  {
-    fetching: false,
-    error: false,
-    data: {
-      books,
-    },
-  },
-])
-jest.mock('@/generated/graphql.client', () => ({
-  __esModule: true,
-  useGetBooksByIdsQuery: () => useGetBooksQueryMock(),
-}))
-
 describe('BookList component', () => {
+  const books = [bookWithImage, bookWithoutImage]
+
   it('本の一覧が表示される', () => {
     const { getByText } = render(<BookList books={books} />)
 
@@ -26,37 +13,8 @@ describe('BookList component', () => {
   })
 
   it('本がない場合は「該当の書籍はありません」というメッセージが表示される', () => {
-    useGetBooksQueryMock.mockReturnValueOnce([
-      {
-        fetching: false,
-        error: false,
-        data: {
-          books: [],
-        },
-      },
-    ])
-
-    const { getByText } = render(<BookList books={books} />)
+    const { getByText } = render(<BookList books={[]} />)
 
     expect(getByText('該当の書籍はありません')).toBeInTheDocument()
-  })
-
-  it('本の一覧の読み込み中は「Loading...」というメッセージが表示される', () => {
-    useGetBooksQueryMock.mockReturnValueOnce([{ fetching: true }])
-
-    const { getByText } = render(<BookList books={books} />)
-
-    expect(getByText('Loading...')).toBeInTheDocument()
-  })
-
-  it('本の一覧の読み込みに失敗した場合、「Error!」というメッセージが表示される', () => {
-    const expectErrorMsg = 'query has errored!'
-    console.error = jest.fn()
-    useGetBooksQueryMock.mockReturnValueOnce([{ fetching: false, error: expectErrorMsg }])
-
-    const { getByText } = render(<BookList books={books} />)
-
-    expect(getByText('Error!')).toBeInTheDocument()
-    expect(console.error).toBeCalledWith(expectErrorMsg)
   })
 })
