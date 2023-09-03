557521b4c03fc4a7e8a4d3e37e921b6fff25b9f4
katakoto291 <kakkookakoo@gmail.com>
2023-09-03 19:00:14 +0900

    感想リストのテストを追加

--- src/app/books/[id]/impressionList.tsx
+++ src/app/books/[id]/impressionList.tsx
@@ -12,45 +12,38 @@ const ImpressionList = async ({ bookId }: Props) => {
     .findMany({
       where: { bookId: bookId },
       include: { user: true },
+      orderBy: [{ updatedAt: 'desc' }],
     })
     .catch((e) => {
       console.error(e)
       return new Error('Book fetch failed')
     })
-
   if (recentImpressions instanceof Error) {
-    return <div>本の取得に失敗しました。再読み込みしてみてください</div>
-  }
-
-  if (recentImpressions == null) {
-    return <div>その本は存在しないようです</div>
+    return <div>感想の取得に失敗しました。再読み込みしてみてください。</div>
   }
 
   return (
-    <div>
-      <h2>感想</h2>
-      <table className="table w-full">
-        <tbody>
-          {recentImpressions.map((impression, index) => {
-            return (
-              <tr className="hover:hover" key={impression.id}>
-                <td className="w-[15rem]" data-testid={`postedDate-${index}`}>
-                  {DateTime.fromISO(impression.updatedAt.toISOString())
-                    .setZone('Asia/Tokyo')
-                    .toFormat(DATE_FORMAT)}
-                </td>
-                <td className="w-[5rem]" data-testid={`postedUser-${index}`}>
-                  <UserAvatar user={impression.user} />
-                </td>
-                <td className="whitespace-pre-wrap" data-testid={`impression-${index}`}>
-                  {impression.impression}
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table className="table w-full">
+      <tbody>
+        {recentImpressions.map((impression, index) => {
+          return (
+            <tr className="hover:hover" key={impression.id}>
+              <td className="w-[15rem]" data-testid={`postedDate-${index}`}>
+                {DateTime.fromISO(impression.updatedAt.toISOString())
+                  .setZone('Asia/Tokyo')
+                  .toFormat(DATE_FORMAT)}
+              </td>
+              <td className="w-[5rem]" data-testid={`postedUser-${index}`}>
+                <UserAvatar user={impression.user} />
+              </td>
+              <td className="whitespace-pre-wrap" data-testid={`impression-${index}`}>
+                {impression.impression}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
 
