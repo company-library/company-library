29c7a2bf8578afff53dab7d33c523704e30cfe90
mongolyy <mongolyy@gmail.com>
2023-09-03 15:34:56 +0900

    useCardでreadingHistories関数を使用するように変更

--- test/__utils__/data/lendingHistory.ts
+++ test/__utils__/data/lendingHistory.ts
@@ -1,5 +1,5 @@
 import { LendingHistory } from '@/models/lendingHistory'
-import { book1, book2, book3 } from './book'
+import { book1, book2, book3, book4, book5, book6 } from './book'
 import { user1 } from './user'
 
 export const lendingHistory1: LendingHistory = {
@@ -21,6 +21,30 @@ export const lendingHistory2: LendingHistory = {
 export const lendingHistory3: LendingHistory = {
   id: 3,
   bookId: book3.id,
+  userId: user1.id,
+  lentAt: new Date('2020-01-01'),
+  dueDate: new Date('2020-02-01'),
+}
+
+export const lendingHistory4: LendingHistory = {
+  id: 4,
+  bookId: book4.id,
+  userId: user1.id,
+  lentAt: new Date('2020-01-01'),
+  dueDate: new Date('2020-02-01'),
+}
+
+export const lendingHistory5: LendingHistory = {
+  id: 5,
+  bookId: book5.id,
+  userId: user1.id,
+  lentAt: new Date('2020-01-01'),
+  dueDate: new Date('2020-02-01'),
+}
+
+export const lendingHistory6: LendingHistory = {
+  id: 6,
+  bookId: book6.id,
   userId: user1.id,
   lentAt: new Date('2020-01-01'),
   dueDate: new Date('2020-02-01'),
