34aa96321302dbd6a6d528d09ae182751ca938b7
katakoto291 <kakkookakoo@gmail.com>
2023-09-03 19:00:40 +0900

    借りている人リストのテストを追加

--- src/app/books/[id]/lendingList.tsx
+++ src/app/books/[id]/lendingList.tsx
@@ -12,48 +12,41 @@ const LendingList = async ({ bookId }: Props) => {
     .findMany({
       where: { bookId: bookId, returnHistory: null },
       include: { user: true },
+      orderBy: [{ lentAt: 'asc' }],
     })
     .catch((e) => {
       console.error(e)
       return new Error('Book fetch failed')
     })
-
   if (lendingHistories instanceof Error) {
-    return <div>本の取得に失敗しました。再読み込みしてみてください</div>
-  }
-
-  if (lendingHistories == null) {
-    return <div>その本は存在しないようです</div>
+    return <div>貸出履歴の取得に失敗しました。再読み込みしてみてください。</div>
   }
 
   return (
-    <div>
-      <h2>借りている人</h2>
-      <table className="table w-full">
-        <tbody>
-          {lendingHistories.map((lendingHistory, index) => {
-            const dueDate = DateTime.fromISO(lendingHistory.dueDate.toISOString())
-            const isOver = dueDate.diff(DateTime.now(), 'days').days <= -1
+    <table className="table w-full">
+      <tbody>
+        {lendingHistories.map((lendingHistory, index) => {
+          const dueDate = DateTime.fromISO(lendingHistory.dueDate.toISOString())
+          const isOver = dueDate.diff(DateTime.now(), 'days').days <= -1
 
-            return (
-              <tr className="hover:hover" key={lendingHistory.id}>
-                <td className="w-[15rem]">
-                  <span
-                    className={isOver ? 'text-red-400 font-bold' : ''}
-                    data-testid={`dueDate-${index}`}
-                  >
-                    {dueDate.setZone('Asia/Tokyo').toFormat(DATE_FORMAT)}
-                  </span>
-                </td>
-                <td data-testid={`lendingUser-${index}`}>
-                  <UserAvatar user={lendingHistory.user} />
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+          return (
+            <tr className="hover:hover" key={lendingHistory.id}>
+              <td className="w-[15rem]">
+                <span
+                  className={isOver ? 'text-red-400 font-bold' : ''}
+                  data-testid={`dueDate-${index}`}
+                >
+                  {dueDate.setZone('Asia/Tokyo').toFormat(DATE_FORMAT)}
+                </span>
+              </td>
+              <td data-testid={`lendingUser-${index}`}>
+                <UserAvatar user={lendingHistory.user} />
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
 
