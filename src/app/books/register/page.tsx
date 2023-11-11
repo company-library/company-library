ae240001abcb16e8f54690c52ea469db0b499ac2
mongolyy <mongolyy@gmail.com>
2023-11-11 19:57:34 +0900

    src/app/books/register/page.tsx のテストの追加

--- src/app/books/register/page.tsx
+++ src/app/books/register/page.tsx
@@ -3,7 +3,6 @@ import Headline from '@/components/common/headline'
 import BookForm from '@/app/books/register/bookForm'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import React from 'react'
 
 // Next.jsでメタデータを設定した場合のテストに問題があるようなので、一旦コメントアウト
 // https://github.com/vercel/next.js/issues/47299#issuecomment-1477912861
--- test/app/books/register/page.test.tsx
+++ test/app/books/register/page.test.tsx
@@ -1,11 +1,23 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { user1 } from '../../../__utils__/data/user'
+
+const getServerSessionMock = jest.fn().mockReturnValue({ customUser: { id: user1.id } })
+jest.mock('next-auth', () => ({
+  __esModule: true,
+  getServerSession: () => getServerSessionMock(),
+}))
+
+jest.mock('@/app/api/auth/[...nextauth]/route', () => ({
+  __esModule: true,
+  authOptions: {},
+}))
 
 describe('register page', () => {
   const RegisterPage = require('@/app/books/register/page').default
 
-  it('13桁入力するとGoogleBookのコンポーネントが表示される', () => {
-    const { getByText } = render(<RegisterPage />)
+  it('13桁入力するとGoogleBookのコンポーネントが表示される', async () => {
+    render(await RegisterPage())
 
-    expect(getByText('本を登録')).toBeInTheDocument()
+    expect(screen.getByText('本を登録')).toBeInTheDocument()
   })
 })
