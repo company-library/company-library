770e2bae017cd955874bf2b2f5182977fb94053a
mongolyy <mongolyy@gmail.com>
2023-11-11 20:32:30 +0900

    src/app/books/register/bookForm.tsx のテストの追加

--- test/app/books/register/bookForm.test.tsx
+++ test/app/books/register/bookForm.test.tsx
@@ -1,34 +1,36 @@
-import GoogleBook from '@/components/googleBook'
 import { fireEvent, render } from '@testing-library/react'
+import { user1 } from '../../../__utils__/data/user'
+import SearchedBook from '@/app/books/register/searchedBook'
 
-jest.mock('@/components/layout')
-jest.mock('@/components/googleBook')
+jest.mock('@/app/books/register/searchedBook')
 
 describe('book form component', () => {
-  ;(GoogleBook as jest.Mock).mockImplementation(() => {
-    return <>google book component</>
+  ;(SearchedBook as jest.Mock).mockImplementation(() => {
+    return <>searched book component</>
   })
 
   const BookFormComponent = require('@/app/books/register/bookForm').default
 
   it('13桁入力するとGoogleBookのコンポーネントが表示される', () => {
-    const { getByText, queryByText, getByLabelText } = render(<BookFormComponent />)
+    const { getByText, queryByText, getByLabelText } = render(
+      <BookFormComponent userId={user1.id} />,
+    )
 
     const input = getByLabelText('ISBN（13桁）を入力してください')
     // 12桁だと表示されない
     fireEvent.change(input, {
       target: { value: '123456789012' },
     })
-    expect(queryByText('google book component')).not.toBeInTheDocument()
+    expect(queryByText('searched book component')).not.toBeInTheDocument()
     // 13桁だと表示される
     fireEvent.change(input, {
       target: { value: '1234567890123' },
     })
-    expect(getByText('google book component')).toBeInTheDocument()
+    expect(getByText('searched book component')).toBeInTheDocument()
     // 14桁だと表示されない
     fireEvent.change(input, {
       target: { value: '12345678901234' },
     })
-    expect(queryByText('google book component')).not.toBeInTheDocument()
+    expect(queryByText('searched book component')).not.toBeInTheDocument()
   })
 })
