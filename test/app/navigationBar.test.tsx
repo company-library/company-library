e82531bac8cd95bcf92e91a18000adff11997d6c
katakoto291 <kakkookakoo@gmail.com>
2023-11-26 18:43:30 +0900

    update: `screen` と `getByRole` を用いるよう変更

--- test/app/navigationBar.test.tsx
+++ test/app/navigationBar.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { user1 } from '../__utils__/data/user'
 
 const pathnameMock = jest.fn().mockReturnValue({ push: jest.fn() })
@@ -23,49 +23,49 @@ describe('navigationBar component', () => {
   const NavigationBar = require('@/app/navigationBar').default
 
   it('ナビゲーション項目が表示される', async () => {
-    const { getByText } = render(await NavigationBar())
+    render(await NavigationBar())
 
-    expect(getByText('company-library')).toBeInTheDocument()
-    expect(getByText('書籍一覧')).toBeInTheDocument()
-    expect(getByText('書籍一覧')).not.toHaveClass('bg-gray-600')
-    expect(getByText('登録')).toBeInTheDocument()
-    expect(getByText('登録')).not.toHaveClass('bg-gray-600')
-    expect(getByText('マイページ')).toBeInTheDocument()
-    expect(getByText('マイページ')).not.toHaveClass('bg-gray-600')
-    expect(getByText('利用者一覧')).toBeInTheDocument()
-    expect(getByText('利用者一覧')).not.toHaveClass('bg-gray-600')
+    expect(screen.getByText('company-library')).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '書籍一覧' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '書籍一覧' })).not.toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: '登録' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '登録' })).not.toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: 'マイページ' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: 'マイページ' })).not.toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: '利用者一覧' })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: '利用者一覧' })).not.toHaveClass('bg-gray-600')
   })
 
   it('pathが/の場合、書籍一覧ボタンのデザインが強調される', async () => {
     pathnameMock.mockReturnValue('/')
 
-    const { getByText } = render(await NavigationBar())
+    render(await NavigationBar())
 
-    expect(getByText('書籍一覧')).toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: '書籍一覧' })).toHaveClass('bg-gray-600')
   })
 
   it('pathが/books/registerの場合、登録ボタンのデザインが強調される', async () => {
     pathnameMock.mockReturnValue('/books/register')
 
-    const { getByText } = render(await NavigationBar())
+    render(await NavigationBar())
 
-    expect(getByText('登録')).toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: '登録' })).toHaveClass('bg-gray-600')
   })
 
   it('pathが/usersの場合、利用者一覧ボタンのデザインが強調される', async () => {
     pathnameMock.mockReturnValue('/users')
 
-    const { getByText } = render(await NavigationBar())
+    render(await NavigationBar())
 
-    expect(getByText('利用者一覧')).toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: '利用者一覧' })).toHaveClass('bg-gray-600')
   })
 
   it('pathが/users/ログインユーザーのidの場合、マイページボタンのデザインが強調される', async () => {
     pathnameMock.mockReturnValue(`/users/${loggedInUser.id}`)
 
-    const { getByText, rerender } = render(await NavigationBar())
+    const { rerender } = render(await NavigationBar())
 
-    expect(getByText('マイページ')).toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: 'マイページ' })).toHaveClass('bg-gray-600')
 
     // ログインユーザー以外のIDの場合強調されない
     pathnameMock.mockReturnValue({
@@ -73,6 +73,6 @@ describe('navigationBar component', () => {
       asPath: `/users/${loggedInUser.id + 1}`,
     })
     rerender(await NavigationBar())
-    expect(getByText('マイページ')).not.toHaveClass('bg-gray-600')
+    expect(screen.getByRole('link', { name: 'マイページ' })).not.toHaveClass('bg-gray-600')
   })
 })
