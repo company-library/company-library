1bce71b8e1d6ccebaedb4bd3b2a04c8bd4ad83e9
mongolyy <mongolyy@gmail.com>
2023-11-26 22:56:28 +0900

    メタデータのコメントアウトを外す

--- src/app/books/register/page.tsx
+++ src/app/books/register/page.tsx
@@ -1,15 +1,13 @@
-import { type NextPage } from 'next'
+import { Metadata, type NextPage } from 'next'
 import Headline from '@/components/common/headline'
 import BookForm from '@/app/books/register/bookForm'
 import { getServerSession } from 'next-auth'
 
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
 
-// Next.jsでメタデータを設定した場合のテストに問題があるようなので、一旦コメントアウト
-// https://github.com/vercel/next.js/issues/47299#issuecomment-1477912861
-// export const metadata: Metadata = {
-//   title: '本を登録 | company-library',
-// }
+export const metadata: Metadata = {
+  title: '本を登録 | company-library',
+}
 
 /**
  * 書籍登録するページ
