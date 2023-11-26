f10d2b77c9a9d5d215e50f2c9501ac5a1c0378f6
mongolyy <mongolyy@gmail.com>
2023-11-26 15:51:29 +0000

    時間の設定を変更

--- test/components/lendButton.test.tsx
+++ test/components/lendButton.test.tsx
@@ -11,7 +11,7 @@ window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverM
 
 const dateFormat = 'yyyy-MM-dd'
 const today = DateTime.now().setZone('Asia/Tokyo')
-const expectedInitialDuDate = today.plus({ days: 7 }).toFormat(dateFormat)
+const expectedInitialDueDate = today.plus({ days: 7 }).toFormat(dateFormat)
 const lendMock = jest.fn()
 const handleDueDateMock = jest.fn()
 const useLendMock = jest.fn().mockReturnValue({
@@ -34,7 +34,7 @@ describe('LendButton component', () => {
     )
 
     expect(getByRole('button', { name: '借りる' })).toBeDisabled()
-    expect(useLendMock).toBeCalledWith(bookId, userId, expectedInitialDuDate)
+    expect(useLendMock).toBeCalledWith(bookId, userId, expectedInitialDueDate)
 
     rerender(<LendButton bookId={bookId} userId={userId} disabled={false} />)
 
