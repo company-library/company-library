88fe6a5278d295ba898d5491c62e27d32322492d
mongolyy <mongolyy@gmail.com>
2023-12-30 13:19:56 +0900

    失敗するケースを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import NavigationBar from '@/app/navigationBar'
 
 import '../styles/globals.css'
 
-export const metadata = {
-  title: 'company-library',
-}
+export const metadata = { title: 'company-library', }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
