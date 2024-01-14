1ff8aa134467945c36b0278cb7df8ab034f23758
katakoto291 <kakkookakoo@gmail.com>
2024-01-14 14:38:13 +0900

    update: Linkの有無の違いのみのため、共通化する

--- src/components/bookTile.tsx
+++ src/components/bookTile.tsx
@@ -11,25 +11,18 @@ type BookProps = {
 }
 
 const BookTile: FC<BookProps> = ({ book }) => {
-  if (book.id) {
-    return (
-      <Link href={`/books/${book.id}`} className="cursor-pointer hover:shadow-2xl">
-        <div className="w-[300px] rounded-lg shadow-lg">
-          <div className="h-[400px] overflow-hidden">
-            <Image
-              src={book.imageUrl ? book.imageUrl : '/no_image.jpg'}
-              alt={book.title}
-              width={300}
-              height={400}
-              data-testid="bookImg"
-            />
-          </div>
-          <p className="text-sm font-medium p-2 truncate">{book.title}</p>
-        </div>
-      </Link>
-    )
-  }
+  return book.id ? (
+    <Link href={`/books/${book.id}`} className="cursor-pointer hover:shadow-2xl">
+      <Tile book={book} />
+    </Link>
+  ) : (
+    <Tile book={book} />
+  )
+}
 
+export default BookTile
+
+const Tile: FC<BookProps> = ({ book }) => {
   return (
     <div className="w-[300px] rounded-lg shadow-lg">
       <div className="h-[400px] overflow-hidden">
@@ -45,5 +38,3 @@ const BookTile: FC<BookProps> = ({ book }) => {
     </div>
   )
 }
-
-export default BookTile
