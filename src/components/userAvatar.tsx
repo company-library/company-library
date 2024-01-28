e1313eaa55940c65b3c0354aafa5b2a9a16de519
katakoto291 <kakkookakoo@gmail.com>
2024-01-28 14:50:34 +0900

    feat: アバターの大きさを変更できるようにする

--- src/components/userAvatar.tsx
+++ src/components/userAvatar.tsx
@@ -7,16 +7,18 @@ type UserAvatarProps = {
     name: string
     email: string
   }
+  size?: Size
 }
 
-const UserAvatar: FC<UserAvatarProps> = async ({ user }) => {
+const UserAvatar: FC<UserAvatarProps> = async ({ user, size = 'md' }) => {
   const imageUrl = await getAvatarUrl(user.email)
+  const width = getWidth(size)
 
   return (
     <div className="tooltip" data-tip={user.name} data-testid="name-tooltip">
       {imageUrl ? (
         <div className="avatar">
-          <div className="w-12">
+          <div className={`${width}`}>
             <Image
               src={imageUrl}
               alt={`${user.name}のプロフィール画像`}
@@ -28,7 +30,7 @@ const UserAvatar: FC<UserAvatarProps> = async ({ user }) => {
         </div>
       ) : (
         <div className="avatar placeholder">
-          <div className="w-12 rounded-full bg-neutral text-neutral-content">
+          <div className={`${width} rounded-full bg-neutral text-neutral-content`}>
             <span>{user.name.substring(0, 1)}</span>
           </div>
         </div>
@@ -38,3 +40,20 @@ const UserAvatar: FC<UserAvatarProps> = async ({ user }) => {
 }
 
 export default UserAvatar
+
+type Size =
+  | 'sm' // w-8
+  | 'md' // w-12
+  | 'lg' // w-16
+const getWidth = (size: Size) => {
+  switch (size) {
+    case 'sm':
+      return 'w-8'
+    case 'md':
+      return 'w-12'
+    case 'lg':
+      return 'w-16'
+    default:
+      return ''
+  }
+}
