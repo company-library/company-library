7aa69669f228a598686c341d12d1821ada8114d2
katakoto291 <kakkookakoo@gmail.com>
2025-06-07 10:13:19 +0900

    コメントで対象を記載

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,14 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 }
