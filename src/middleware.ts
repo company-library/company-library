bd93e1b6babf705eb434281e7060eaa439ce07ef
katakoto291 <kakkookakoo@gmail.com>
2025-06-07 10:06:09 +0900

    追加したmiddlewareではなく、公式ページを参照したmatcherの設定で `/api` 以下は認証不要になっていただけなので、withAuthの部分は元に戻す

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,10 @@
 import { withAuth } from 'next-auth/middleware'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-const PUBLIC_PATHS = ['/api/cron']
-
-export default withAuth(
-  function middleware(req: NextRequest) {
-    const { pathname } = req.nextUrl
-
-    if (PUBLIC_PATHS.some((path) => pathname.startsWith(path))) {
-      return NextResponse.next()
-    }
-
-    return NextResponse.next()
+export default withAuth({
+  pages: {
+    signIn: '/auth/signIn',
   },
-  {
-    pages: {
-      signIn: '/auth/signIn',
-    },
-  },
-)
+})
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
