c62351770335a425c884751a2e33e052b8c1f0b3
mongolyy <mongolyy@gmail.com>
2025-07-28 23:15:10 +0900

    書籍の詳細ページでユーザーの表示をリンクにする

--- src/app/books/[id]/returnList.test.tsx
+++ src/app/books/[id]/returnList.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import { Suspense } from 'react'
 import ReturnList from '@/app/books/[id]/returnList'
 import { lendableBook } from '../../../../test/__utils__/data/book'
 import { prismaMock } from '../../../../test/__utils__/libs/prisma/singleton'
@@ -7,7 +6,11 @@ import { prismaMock } from '../../../../test/__utils__/libs/prisma/singleton'
 describe('ReturnList Component', async () => {
   const { UserAvatarMock } = vi.hoisted(() => {
     return {
-      UserAvatarMock: vi.fn().mockImplementation(({ user }) => <div>{user.name}</div>),
+      UserAvatarMock: vi.fn().mockImplementation(({ user, linkToProfile }) => (
+        linkToProfile ? 
+          <a href={`/users/${encodeURIComponent(user.email)}`}>{user.name}</a> :
+          <div>{user.name}</div>
+      )),
     }
   })
   vi.mock('@/components/userAvatar', () => ({
@@ -22,7 +25,7 @@ describe('ReturnList Component', async () => {
         id: 2,
         dueDate: new Date('2022-10-08'),
         lentAt: new Date('2022-10-01'),
-        user: { id: 2, name: 'user02' },
+        user: { id: 2, name: 'user02', email: 'user02@example.com' },
       },
     },
     {
@@ -32,7 +35,7 @@ describe('ReturnList Component', async () => {
         id: 3,
         dueDate: new Date('2022-10-15'),
         lentAt: new Date('2022-10-01'),
-        user: { id: 3, name: 'user03' },
+        user: { id: 3, name: 'user03', email: 'user03@example.com' },
       },
     },
     {
@@ -42,7 +45,7 @@ describe('ReturnList Component', async () => {
         id: 1,
         dueDate: new Date('2022-10-24'),
         lentAt: new Date('2022-10-01'),
-        user: { id: 1, name: 'user01' },
+        user: { id: 1, name: 'user01', email: 'user01@example.com' },
       },
     },
   ]
@@ -51,14 +54,9 @@ describe('ReturnList Component', async () => {
   prismaReturnHistoryMock.mockResolvedValue(expectedReturnHistories)
 
   it('返却済の貸出履歴がある場合、その一覧が返却日の昇順で表示される', async () => {
-    render(
-      <Suspense>
-        <ReturnList bookId={lendableBook.id} />
-      </Suspense>,
-    )
+    render(await ReturnList({ bookId: lendableBook.id }))
 
-    // Suspenseの解決を待つために、最初のテスト項目のみawaitを使う
-    expect((await screen.findByTestId(`returnedDate-${0}`)).textContent).toBe(
+    expect(screen.getByTestId(`returnedDate-${0}`).textContent).toBe(
       '2022/10/01〜2022/10/30',
     )
     expect(screen.getByTestId(`returnedUser-${0}`).textContent).toBe(
@@ -79,14 +77,9 @@ describe('ReturnList Component', async () => {
     // @ts-ignore
     prismaReturnHistoryMock.mockResolvedValue([])
 
-    render(
-      <Suspense>
-        <ReturnList bookId={lendableBook.id} />
-      </Suspense>,
-    )
+    render(await ReturnList({ bookId: lendableBook.id }))
 
-    // Suspenseの解決を待つために、最初のテスト項目のみawaitを使う
-    expect(await screen.findByText('これまで借りたユーザーはいません')).toBeInTheDocument()
+    expect(screen.getByText('これまで借りたユーザーはいません')).toBeInTheDocument()
   })
 
   it('返却履歴の取得時にエラーが発生した場合、エラーメッセージが表示される', async () => {
@@ -94,16 +87,25 @@ describe('ReturnList Component', async () => {
     prismaReturnHistoryMock.mockRejectedValue(expectedError)
     console.error = vi.fn()
 
-    render(
-      <Suspense>
-        <ReturnList bookId={lendableBook.id} />
-      </Suspense>,
-    )
+    render(await ReturnList({ bookId: lendableBook.id }))
 
-    // Suspenseの解決を待つために、最初のテスト項目のみawaitを使う
     expect(
-      await screen.findByText('返却履歴の取得に失敗しました。再読み込みしてみてください。'),
+      screen.getByText('返却履歴の取得に失敗しました。再読み込みしてみてください。'),
     ).toBeInTheDocument()
     expect(console.error).toBeCalledWith(expectedError)
+  })
+
+  it('各ユーザーアバターがリンクになっている', async () => {
+    // @ts-ignore
+    prismaReturnHistoryMock.mockResolvedValue(expectedReturnHistories)
+
+    render(await ReturnList({ bookId: lendableBook.id }))
+
+    const userLinks = screen.getAllByRole('link')
+    
+    expect(userLinks).toHaveLength(3)
+    expect(userLinks[0]).toHaveAttribute('href', `/users/${encodeURIComponent(expectedReturnHistories[0].lendingHistory.user.email)}`)
+    expect(userLinks[1]).toHaveAttribute('href', `/users/${encodeURIComponent(expectedReturnHistories[1].lendingHistory.user.email)}`)
+    expect(userLinks[2]).toHaveAttribute('href', `/users/${encodeURIComponent(expectedReturnHistories[2].lendingHistory.user.email)}`)
   })
 })
