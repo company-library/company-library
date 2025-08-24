2b56a306e92bb27313cdcfc2a710575e1b27ef74
mongolyy <mongolyy@gmail.com>
2025-08-24 21:39:29 +0900

    テストの失敗に対応

--- src/app/admin/books/actions.test.ts
+++ src/app/admin/books/actions.test.ts
@@ -1,3 +1,4 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { prismaMock } from '../../../../test/__utils__/libs/prisma/singleton'
 import { getBooksWithMissingInfo, updateSingleBookInfo } from './actions'
 
--- src/app/books/[id]/bookDetail.test.tsx
+++ src/app/books/[id]/bookDetail.test.tsx
@@ -13,7 +13,6 @@ describe('BookDetail component', () => {
   const bookDetail = {
     id: book.id,
     createdAt: book.createdAt,
-    updatedAt: book.updatedAt,
     title: book.title,
     description: book.description,
     isbn: book.isbn,
