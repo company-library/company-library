7853acfdc54498f707f154662e2baedfbb9bfedd
katakoto291 <kakkookakoo@gmail.com>
2025-12-14 11:05:48 +0900

    PrismaClientの初期化時にconnectionStringを使用するように修正

--- src/libs/prisma/client.ts
+++ src/libs/prisma/client.ts
@@ -1,20 +1,22 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@/generated/prisma/client'
 
-let prisma: PrismaClient;
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`
 
-if (process.env.NODE_ENV === "production") {
-  const adapter = new PrismaPg({});
-  prisma = new PrismaClient({ adapter });
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  const adapter = new PrismaPg({ connectionString })
+  prisma = new PrismaClient({ adapter })
 } else {
   // @ts-expect-error
   if (!global.prisma) {
-    const adapter = new PrismaPg({});
+    const adapter = new PrismaPg({ connectionString })
     // @ts-expect-error
-    global.prisma = new PrismaClient({ adapter });
+    global.prisma = new PrismaClient({ adapter })
   }
   // @ts-expect-error
-  prisma = global.prisma;
+  prisma = global.prisma
 }
 
-export default prisma;
+export default prisma
